d47a131818837ca884f0d85922f0b38e8491b8bd
bennycdr <bkmiles@gmail.com>
2019-12-12 16:13:07 -0500

    fixed event formatting

--- activities_python/events/event_resolver.py
+++ activities_python/events/event_resolver.py
@@ -8,7 +8,7 @@ from activities_python.constants.basic_constants import BasicConstants
 def resolve_event(event_type, options):
     """Return the proper handler based on the event type. """
 
-if event_type == BasicConstants.VERIFY_TARGET_TYPE:
+    if event_type == BasicConstants.VERIFY_TARGET_TYPE:
         handler = VerifyTargetQuery()
     elif event_type == BasicConstants.SELECT_TYPE:
         handler = MySQLSelect()
